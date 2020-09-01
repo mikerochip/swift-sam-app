77185f2a02c5ede1a3a6f861ebee99820d43a51f
Mike Schweitzer <mschweitzer@gmail.com>
2020-09-01 00:22:29 -0700

    Filled out Package.swift

--- SwiftSamApp.Api/Package.swift
+++ SwiftSamApp.Api/Package.swift
@@ -1,22 +1,25 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftSamApp",
+    platforms: [
+        .macOS(.v10_13),
+    ],
+    products: [
+        .executable(name: "SwiftSamApp", targets: ["SwiftSamApp"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "0.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftSamApp",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftSamAppTests",
-            dependencies: ["SwiftSamApp"]),
+        .target(name: "SwiftSamApp", dependencies: [
+            .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+            .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
+        ]),
+        .testTarget(name: "SwiftSamAppTests", dependencies: [
+            "SwiftSamApp"
+        ]),
     ]
 )
