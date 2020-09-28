ab4e437d658b700d26c4163fd3a00e1ac26901eb
Mike Schweitzer <mschweitzer@gmail.com>
2020-09-27 23:24:19 -0700

    Lambda handler only handles supported routes now

--- SwiftSamApp.Api/Sources/SwiftSamApp/APIGatewayProxyLambda.swift
+++ SwiftSamApp.Api/Sources/SwiftSamApp/APIGatewayProxyLambda.swift
@@ -8,12 +8,25 @@ struct APIGatewayProxyLambda: EventLoopLambdaHandler {
 
     public func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
         print("Entered lambda handler \(context.requestID)")
-        
-        let response = Out(
-            statusCode: .ok,
-            body: "Hello, world!")
-        
+        let response = handleRoute(event)
         print("Exiting lambda handler \(context.requestID)")
         return context.eventLoop.makeSucceededFuture(response)
+    }
+    
+    private func handleRoute(_ event: In) -> Out {
+        let path = event.context.http.path
+        let method = event.context.http.method
+        
+        if path == "/" {
+            if method == HTTPMethod.GET {
+                return Out(statusCode: .ok, body: handleHello(event.body))
+            }
+        }
+        
+        return Out(statusCode: .notFound, body: "Invalid Route")
+    }
+    
+    private func handleHello(_ body: String?) -> String {
+        return "Hello, world!"
     }
 }
