288cb23d5ae6086a8adafba6273e03c9e9b109d9
Mike Schweitzer <mschweitzer@gmail.com>
2020-09-27 17:53:26 -0700

    Attempting see wtf is going wrong by logging

--- SwiftSamApp.Api/Sources/SwiftSamApp/main.swift
+++ SwiftSamApp.Api/Sources/SwiftSamApp/main.swift
@@ -1,5 +1,9 @@
 import AWSLambdaRuntime
 
-Lambda.run { (_: Lambda.Context, _: String, callback: (Result<String, Error>) -> Void) in
+print("I'm alive!")
+
+Lambda.run { (context: Lambda.Context, _: String, callback: (Result<String, Error>) -> Void) in
+    context.logger.debug("Entered lambda handler")
     callback(.success("Hello, world!"))
+    context.logger.debug("Exited lambda handler")
 }
