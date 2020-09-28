1ceac011af073bb3e6e0cc58f046291d94ab5ad2
Mike Schweitzer <mschweitzer@gmail.com>
2020-09-27 23:04:33 -0700

    Changed lambda handler to actually use typealiases

--- SwiftSamApp.Api/Sources/SwiftSamApp/APIGatewayProxyLambda.swift
+++ SwiftSamApp.Api/Sources/SwiftSamApp/APIGatewayProxyLambda.swift
@@ -6,10 +6,10 @@ struct APIGatewayProxyLambda: EventLoopLambdaHandler {
     public typealias In = APIGateway.V2.Request
     public typealias Out = APIGateway.V2.Response
 
-    public func handle(context: Lambda.Context, event: APIGateway.V2.Request) -> EventLoopFuture<APIGateway.V2.Response> {
+    public func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
         print("Entered lambda handler \(context.requestID)")
         
-        let response = APIGateway.V2.Response(
+        let response = Out(
             statusCode: .ok,
             body: "Hello, world!")
         
