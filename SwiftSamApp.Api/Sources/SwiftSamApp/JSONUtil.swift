9c7d2a6843bedebc61f57e60861d184c09176c40
Mike Schweitzer <mschweitzer@gmail.com>
2020-09-29 00:53:54 -0700

    Attempting to deserialize and reserialize json to get actual json formatted logs

--- SwiftSamApp.Api/Sources/SwiftSamApp/JSONUtil.swift
+++ SwiftSamApp.Api/Sources/SwiftSamApp/JSONUtil.swift
@@ -1,13 +1,16 @@
+import Foundation
 import Logging
 
 func printJson(_ str:String) {
-    print(stripNewlines(str))
+    print(convertToJson(str))
 }
 
 func printJson(_ logger:Logger, _ str:String) {
-    logger.info("\(stripNewlines(str))")
+    logger.info("\(convertToJson(str))")
 }
 
-func stripNewlines(_ str:String) -> String {
-    return str.replacingOccurrences(of: "\n", with: "")
+func convertToJson(_ str:String) -> String {
+    let json = try! JSONSerialization.jsonObject(with: str.data(using: .utf8)!)
+    let jsonData = try! JSONEncoder().encode(json as! [String:String])
+    return String(data: jsonData, encoding: .utf8)!
 }
