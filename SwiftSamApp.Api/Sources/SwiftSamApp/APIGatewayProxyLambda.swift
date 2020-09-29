3ff779b4fff6a07bf43e6f0fdbc9919395ab72c6
Mike Schweitzer <mschweitzer@gmail.com>
2020-09-28 21:56:07 -0700

    Attempting to get logs to show up in CWL

--- SwiftSamApp.Api/Sources/SwiftSamApp/APIGatewayProxyLambda.swift
+++ SwiftSamApp.Api/Sources/SwiftSamApp/APIGatewayProxyLambda.swift
@@ -13,9 +13,9 @@ struct APIGatewayProxyLambda: EventLoopLambdaHandler {
     }
     
     public func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
-        print("Entered lambda handler \(context.requestID)")
+        context.logger.debug("Entered lambda handler \(context.requestID)")
         let response = handleRoute(event)
-        print("Exiting lambda handler \(context.requestID)")
+        context.logger.debug("Exiting lambda handler \(context.requestID)")
         return context.eventLoop.makeSucceededFuture(response)
     }
     
