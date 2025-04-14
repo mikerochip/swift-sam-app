2ec338d6387188da0035f1b4bf4e1a43762fbc02
Mike Schweitzer <mschweitzer@gmail.com>
2025-04-13 23:36:34 -0700

    More fix compile errors using HTTPRequest

--- SwiftSamApp.Api/Package.swift
+++ SwiftSamApp.Api/Package.swift
@@ -11,11 +11,13 @@ let package = Package(
         .executable(name: "SwiftSamApp", targets: ["SwiftSamApp"])
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0"),
         .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0-alpha.3"),
         .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(name: "SwiftSamApp", dependencies: [
+            .product(name: "HTTPTypes", package: "swift-http-types"),
             .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
             .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
         ]),
