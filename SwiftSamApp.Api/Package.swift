ac1a369030d3a082bbbc18cc96f93d3ffbb17690
Mike Schweitzer <mschweitzer@gmail.com>
2025-04-13 23:11:37 -0700

    NOICE we're getting somewhere

--- SwiftSamApp.Api/Package.swift
+++ SwiftSamApp.Api/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0-alpha.3"),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", branch: "v1.0.0"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "v1.0.0"),
     ],
     targets: [
         .executableTarget(name: "SwiftSamApp", dependencies: [
